7d88ba1b81af77a5ba7ccac83c676ebf1be3cda6
student00010671 <student00010671@gmail.com>
2021-03-19 11:49:01 -0400

    tw2

---
+++ WIUT/WIUT/Applicant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIUT
+{
+    class Applicant
+    {
+        private int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public DateTime DoB { get; set; }
+        public string MaritalStatus { get; set; }
+        public string PassportNo { get; set; }
+        public Course Course { get; set; }
+
+        public Applicant(string name, string surname, string address, DateTime dob, string maritalstatus, string passportNo, Course course)
+        {
+            Name = name;
+            Surname = surname;
+            Address = address;
+            DoB = dob;
+            MaritalStatus = maritalstatus;
+            PassportNo = passportNo;
+            Course = course;
+        }
+
+        public Applicant (string name, string surname, Course course)
+        {
+            Name = name;
+            Surname = surname;
+            Course = course;
+        }
+
+    }
+}
---
+++ WIUT/WIUT/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIUT
+{
+    class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Course()
+        {
+
+        }
+
+        public Course(string name) 
+        {
+            Name = name;
+        }
+
+    }
+}
